9bfed4c9b53b8a6c85d63dbe5e9fcc8ae624df4f
Adrian Cano <adrian.cano@manpowergroup.com.co>
2024-01-26 17:13:30 -0500

    Config laravel mix and initial setup

---
+++ webpack.mix.js
@@ -0,0 +1,23 @@
+// webpack.mix.js
+
+let mix = require('laravel-mix');
+
+if (process.env.npm_lifecycle_event !== 'watch') {
+    mix.combine(
+        ['./node_modules/normalize.css/normalize.css'],
+        'dist/css/vendors.css'
+    );
+
+    mix.copy('assets/fonts', 'dist/fonts');
+}
+
+mix.js('src/js/app.js', 'js')
+    .sass('src/sass/app.scss', 'css')
+    .options({
+        processCssUrls: false,
+    })
+    .setPublicPath('dist');
+
+if (mix.inProduction()) {
+    mix.sourceMaps().version();
+}

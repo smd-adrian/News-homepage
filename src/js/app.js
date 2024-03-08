d6d00dc54ea5748cd0e41fc67a90e45821ec10ca
Adrian Cano <adriancanoj1994@gmail.com>
2024-03-08 14:47:03 -0500

    Star responsive

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,17 @@
 console.log('Starting');
+
+// function myFunction(x) {
+//     x.classList.toggle('change');
+// }
+
+// Obtén el elemento por su id
+// let mobileIcon = document.getElementById('mobile-icon-menu');
+
+// // Agrega un evento de clic al elemento
+// mobileIcon.addEventListener('click', function () {
+//     // Ejecuta el código toggle al hacer clic
+//     this.classList.toggle('change');
+// });
+
+// let mainMenu = document.querySelectorAll('.main-menu');
+// mainMenu.style.display = 'block';
